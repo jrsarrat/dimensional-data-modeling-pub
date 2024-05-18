193dbef479b97734c3982a7be8e221b3495263bd
jrsarrat <151387051+jrsarrat@users.noreply.github.com>
2024-05-17 23:44:51 -0400

    Update query_1.sql

---
+++ submission/query_1.sql
@@ -0,0 +1,19 @@
+CREATE TABLE jrsarrat.actors ( --create an actors table with applicable data types
+  actor VARCHAR, 
+  actor_id VARCHAR, 
+  films ARRAY(ROW( --this struct provides a table of data consisting of film attributes
+    film VARCHAR,
+    year INTEGER,
+    votes INTEGER,
+    rating DOUBLE,
+    film_id VARCHAR
+  )), 
+  quality_class VARCHAR,
+  is_active BOOLEAN,
+  current_year INTEGER 
+)
+WITH
+  (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']
+  )
